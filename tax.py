c4c3ec09b6cd8e040afb1ffbe8341e193d6e3f14
Natth M <rm.ratthapong@gmail.com>
2019-08-28 19:45:48 +0700

    add

---
+++ tax.py
@@ -0,0 +1,14 @@
+# r/dailyprogrammer 
+# easy #379
+# Progressive taxation
+# https://www.reddit.com/r/dailyprogrammer/comments/cdieag/20190715_challenge_379_easy_progressive_taxation/
+# write program that calculate tax
+
+
+def tax(income):
+
+
+
+
+tax(18000)
+tax(35000)
