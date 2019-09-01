6a2421ae7092899aa94a0bd206ef2dac5e91b78f
Natth M <rm.ratthapong@gmail.com>
2019-09-01 18:21:39 +0700

    solved 378

--- easy_378.py
+++ easy_378.py
@@ -15,3 +15,31 @@ def warmup2(lst):
   lst_warmup2 = lst
   lst_warmup2.sort(reverse=True)
   return lst_warmup2
+
+def warmup3(number, lst):
+  return bool(number > len(lst))
+
+def warmup4(number, lst):
+  lst_warmup4 = lst
+  for i in range(number):
+    lst_warmup4[i] = lst_warmup4[i] - 1
+  return lst_warmup4
+
+def hh(lst):
+  hh_lst = lst
+
+  while(True):
+    # Eliminate 0's
+    warmup1(hh_lst)
+    # If sequence is empty stop and return True
+    if hh_lst == []:
+      return True
+    # Sort the sequence in descending order
+    warmup2(hh_lst)
+    # Remove the first answer and give it into N
+    n = hh_lst[0]
+    hh_lst.pop(0)
+    if warmup3(n, hh_lst):
+      return False  
+    # Subtract 1 from each of N
+    warmup4(n, hh_lst)
